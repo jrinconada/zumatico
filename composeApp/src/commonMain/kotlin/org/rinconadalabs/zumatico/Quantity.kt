995151d8a3f8cb35bc18b69cf3933224b707f93f
Juan Rinconada <jrinconada@gmail.com>
2025-08-07 14:37:00 +0200

    Add fruit arrangement basic logic

--- composeApp/src/commonMain/kotlin/org/rinconadalabs/zumatico/Quantity.kt
+++ composeApp/src/commonMain/kotlin/org/rinconadalabs/zumatico/Quantity.kt
@@ -12,30 +12,59 @@ import zumatico.composeapp.generated.resources.unknown
 
 class Quantity : Term() {
     var bounds: Rect? = null
+    val termScale = 0.3f
     override val image = mutableStateOf(Res.drawable.unknown)
     private var fruits = mutableSetOf<Fruit>()
 
     val count get() = fruits.size
     fun add(fruit: Fruit) {
-        move(fruit)
         fruits.add(fruit)
+        updateFruits()
     }
     fun remove(fruit: Fruit) {
         fruits.remove(fruit)
     }
 
-    private fun moveFruits() {
-        fruits.forEach { fruit -> move(fruit) }
+    private fun updateFruits() {
+        fruits.forEachIndexed { i, fruit -> move(i, fruit) }
     }
 
-    private fun move(fruit: Fruit) {
-        val termScale = 0.3f
-        val fruitScale = 0.2f
-        val ratio = fruitScale / termScale
+    private fun move(i: Int, fruit: Fruit) {
+        val fruitScale = getFruitScale()
         bounds?.let {
-            val snapX = it.center.x - it.width * ratio / 2f
-            val snapY = it.center.y - it.height * ratio / 2f
+            val snapX = it.center.x + it.width * getXOffset(i)
+            val snapY = it.center.y + it.height * getYOffset(i)
             fruit.goTo(Offset(snapX, snapY), fruitScale)
+        }
+    }
+
+    private fun getFruitScale() : Float {
+        return when(fruits.size) {
+            in 2..4 -> 0.11f
+            in 5..9 -> 0.1f
+            in 10..16 -> 0.05f
+            else -> 0.2f // 1
+        }
+    }
+
+    private fun getXOffset(i: Int) : Float {
+        val fruitScale = getFruitScale()
+        val ratio = fruitScale / termScale
+        return when(fruits.size) {
+            1,2 -> -ratio / 2f
+            3,7 -> if (i == 0) -ratio / 2f else if (i == 1) -ratio else ratio / 2f
+            in 4..9 -> 0f
+            else -> -ratio / 2f
+        }
+    }
+
+    private fun getYOffset(i: Int) : Float {
+        val fruitScale = getFruitScale()
+        val ratio = fruitScale / termScale
+        return when(fruits.size) {
+            1 -> -ratio / 2f
+            in 2..4 -> if (i == 0) -ratio else 0f
+            else -> -ratio / 2f
         }
     }
 
@@ -45,7 +74,7 @@ class Quantity : Term() {
     override fun Draw() {
         drawing(Modifier.onGloballyPositioned { coordinates ->
             bounds = coordinates.boundsInRoot()
-            moveFruits()
+            updateFruits()
         }, image.value)
     }
 }
